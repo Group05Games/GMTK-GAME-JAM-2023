2fc1ea0c7148f481fda7333930225cd3af961fae
Group05Games <group05games@gmail.com>
2023-07-08 21:44:43 -0400

    Add collisions

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-    public Rigidbody2D projectile;
+    private Rigidbody2D projectile;
     public float moveSpeed = 3.0f;
     
     void Start()
@@ -19,4 +19,35 @@ public class BulletScript : MonoBehaviour
         float y = this.transform.position.y;
         projectile.velocity = new Vector2(0, 1) * moveSpeed;
     }
+
+	public void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.name == "BoundingBoxTop") {
+            print("delete bullet");
+            Destroy(this.gameObject);
+		}
+        else {
+            print("hello");
+		}
+    }
+
+	public void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.gameObject.name == "Legaly Distinct") {
+            print("delete Bat");
+            Destroy(this.gameObject);
+            Destroy(collision.gameObject);
+		}
+        else if (collision.gameObject.name == "Legaly Distinct 2") {
+            print("delete Bee");
+            Destroy(this.gameObject);
+            Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.name == "Legaly Distinct 3") {
+            print("delete Sonic");
+            Destroy(this.gameObject);
+            Destroy(collision.gameObject);
+        }
+        else {
+            print("hello 2");
+		}
+	}
 }
